3c8d7162fe0a082b79a9c387a91eb5cb5490ef17
Harryson Pointdujour <Harryson Pointdujour>
2016-10-19 20:31:41 -0400

    installed cors middleware

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var server = express();
+var logger = require('./middleware/logger.js');
+
 
 var indexRouter = require('./routers/index.router.js');
 
 var port = process.env.PORT || 8080;
 
 server.use(express.static( __dirname + '/public'));
+server.use(logger);
+
 
 server.use(indexRouter);
 
